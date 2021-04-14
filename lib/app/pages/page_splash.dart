11bae36dbc8fd38fbc97527c230e0fc5f8e520c7
MSavioti <angelosavioti@gmail.com>
2021-04-14 01:42:07 -0300

    posicionada imagem na splash screen

---
+++ lib/app/pages/page_splash.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  SplashPage({Key key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: double.infinity,
+      height: double.infinity,
+      child: Center(
+        child: Image.asset(
+          'assets/images/icon_virus_400x387.png',
+          width: 200.0,
+          height: 193.23,
+        ),
+      ),
+    );
+  }
+}
