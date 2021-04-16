a0f4917bb6190bc3dbe1b491ef1e68cecfb8ec3e
MSavioti <angelosavioti@gmail.com>
2021-04-16 02:22:34 -0300

    ajeitando home para testar botões de continente

--- lib/app/pages/page_home.dart
+++ lib/app/pages/page_home.dart
@@ -1,4 +1,5 @@
 import 'package:desafio_covid/app/shared/widgets/appbar_default.dart';
+import 'package:desafio_covid/app/shared/widgets/button_continent.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -7,51 +8,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const int _continentsCount = 6;
     return Scaffold(
       appBar: DefaultAppBar(title: 'Covid-19'),
-      body: ListView(
+      body: ListView.separated(
         padding: EdgeInsets.fromLTRB(14.5, 20.5, 14.5, 20.5),
-        children: [
-          ButtonTheme(
-            minWidth: 345.0,
-            // height: 79.0,
-            child: ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.white),
-              ),
-              onPressed: () => {},
-              child: ListTile(
-                contentPadding: EdgeInsets.fromLTRB(0.0, 4.0, 0.0, 5.0),
-                leading: Image.asset(
-                  'assets/images/icon_asia_44x44.png',
-                  width: 44.0,
-                  height: 44.0,
-                ),
-                title: Text(
-                  'Asia',
-                  style: TextStyle(
-                    fontFamily: 'Ubuntu',
-                    fontStyle: FontStyle.normal,
-                    fontSize: 13.0,
-                    color: Color(0xFF1E2243),
-                  ),
-                ),
-                subtitle: Text(
-                  '45 países',
-                  style: TextStyle(
-                    fontFamily: 'Ubuntu',
-                    fontStyle: FontStyle.normal,
-                    fontSize: 12.0,
-                    color: Color(0xFF969AA8),
-                  ),
-                ),
-                trailing: Image.asset(
-                  'assets/images/arrow_control_small.png',
-                ),
-              ),
-            ),
-          ),
-        ],
+        itemCount: _continentsCount,
+        itemBuilder: (_, i) {
+          return ContinentButton(
+            countriesCount: 1,
+            continentName: 'Asia',
+          );
+        },
+        separatorBuilder: (_, i) {
+          return Divider(
+            height: 12.0,
+          );
+        },
       ),
     );
   }
