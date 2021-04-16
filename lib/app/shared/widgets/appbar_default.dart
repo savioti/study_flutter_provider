5337a05c84bcde4178bdfbe93b9b2057feb8d79a
MSavioti <angelosavioti@gmail.com>
2021-04-16 01:31:09 -0300

    extraindo appbar e botão do continente

---
+++ lib/app/shared/widgets/appbar_default.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class DefaultAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const DefaultAppBar({Key key, @required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: const Color(0xFFFBFBFD),
+      elevation: 0.0,
+      bottom: PreferredSize(
+        preferredSize: preferredSize,
+        child: Container(
+          height: 1.0,
+          color: Colors.white,
+        ),
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'Ubuntu',
+          fontWeight: FontWeight.bold,
+          fontSize: 18.0,
+          color: Color(0xFF1E2243),
+        ),
+      ),
+      centerTitle: true,
+      leading: Image.asset(
+        'assets/images/icon_virus_30x29.png',
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size(double.infinity, 53.0);
+}
---
+++ lib/app/shared/widgets/button_continent.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class ContinentButton extends StatelessWidget {
+  const ContinentButton({Key key, this.continentName, this.countriesCount})
+      : super(key: key);
+
+  final String continentName;
+  final int countriesCount;
+
+  String _findContinentIcon(String name) {
+    switch (name) {
+      case 'Asia':
+        return 'assets/images/icon_asia_44x44.png';
+      case 'North America':
+        return 'assets/images/icon_america_44x44.png';
+      case 'South America':
+        return 'assets/images/icon_america_44x44.png';
+      case 'Europe':
+        return 'assets/images/icon_europe_44x44.png';
+      case 'Africa':
+        return 'assets/images/icon_africa_44x44.png';
+      case 'Australia/Oceania':
+        return 'assets/images/icon_oceania_44x44.png';
+      default:
+        throw ArgumentError('$name is not a continent.');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ButtonTheme(
+      minWidth: 345.0,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.white),
+        ),
+        onPressed: () => {},
+        child: ListTile(
+          contentPadding: EdgeInsets.fromLTRB(0.0, 4.0, 0.0, 5.0),
+          leading: Image.asset(
+            _findContinentIcon(continentName),
+            width: 44.0,
+            height: 44.0,
+          ),
+          title: Text(
+            continentName,
+            style: TextStyle(
+              fontFamily: 'Ubuntu',
+              fontStyle: FontStyle.normal,
+              fontSize: 13.0,
+              color: Color(0xFF1E2243),
+            ),
+          ),
+          subtitle: Text(
+            '$countriesCount países',
+            style: TextStyle(
+              fontFamily: 'Ubuntu',
+              fontStyle: FontStyle.normal,
+              fontSize: 12.0,
+              color: Color(0xFF969AA8),
+            ),
+          ),
+          trailing: Image.asset(
+            'assets/images/arrow_control_small.png',
+          ),
+        ),
+      ),
+    );
+  }
+}
