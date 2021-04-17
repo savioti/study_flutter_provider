37752fe7a082b50ad3675f8d138938beb3c64db0
MSavioti <angelosavioti@gmail.com>
2021-04-17 01:49:46 -0300

    carregando informações dos continentes pela API

--- lib/app/pages/page_home.dart
+++ lib/app/pages/page_home.dart
@@ -1,32 +1,62 @@
+import 'package:desafio_covid/app/shared/models/continent.dart';
 import 'package:desafio_covid/app/shared/widgets/appbar_default.dart';
 import 'package:desafio_covid/app/shared/widgets/button_continent.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
 
+  Future<List<Continent>> _httpGet() async {
+    var response = await Dio()
+        .get('https://disease.sh/v3/covid-19/continents?yesterday=true');
+
+    if (response == null) return null;
+    var jsonContent = List.from(response.data);
+    List<Continent> continents = [];
+    jsonContent.forEach((item) {
+      continents.add(Continent.fromJson(item));
+    });
+    return continents;
+  }
+
   @override
   Widget build(BuildContext context) {
-    const int _continentsCount = 6;
     return Scaffold(
       appBar: DefaultAppBar(title: 'Covid-19'),
-      body: ListView.separated(
-        padding: EdgeInsets.fromLTRB(14.5, 20.5, 14.5, 20.5),
-        itemCount: _continentsCount,
-        itemBuilder: (_, i) {
-          return ContinentButton(
-            countriesCount: 1,
-            continentName: 'Asia',
-          );
-        },
-        separatorBuilder: (_, i) {
-          return Divider(
-            height: 12.0,
-            color: Color(0x00000000),
-          );
-        },
-      ),
+      body: FutureBuilder<List<Continent>>(
+          future: _httpGet(),
+          builder: (_, snapshot) {
+            var continents = snapshot.data;
+            return ContinentsList(continents: continents);
+          }),
+    );
+  }
+}
+
+class ContinentsList extends StatelessWidget {
+  const ContinentsList({Key key, this.continents});
+
+  final List<Continent> continents;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: EdgeInsets.fromLTRB(14.5, 20.5, 14.5, 20.5),
+      itemCount: continents.length,
+      itemBuilder: (_, i) {
+        return ContinentButton(
+          countriesCount: continents[i].countriesNames.length,
+          continentName: continents[i].name,
+        );
+      },
+      separatorBuilder: (_, i) {
+        return Divider(
+          height: 12.0,
+          color: Color(0x00000000),
+        );
+      },
     );
   }
 }
