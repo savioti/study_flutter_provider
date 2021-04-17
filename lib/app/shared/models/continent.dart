14f26c0641fa74a07d466bbfa5776d8147deb4b2
MSavioti <angelosavioti@gmail.com>
2021-04-17 01:48:17 -0300

    adaptando classe de continente para receber dado do json

--- lib/app/shared/models/continent.dart
+++ lib/app/shared/models/continent.dart
@@ -1,27 +1,27 @@
-import 'package:desafio_covid/app/shared/models/country.dart';
-import 'package:flutter/material.dart';
-
 class Continent {
-  const Continent(
-      {@required this.totalCases,
-      @required this.activeCases,
-      @required this.totalRecovered,
-      @required this.totalDeaths,
-      @required this.todayCases,
-      @required this.todayDeaths,
-      @required this.totalTests,
-      @required this.population,
-      @required this.countries});
+  Continent.fromJson(Map<String, dynamic> json) {
+    name = json['continent'];
+    totalCases = json['cases'];
+    activeCases = json['active'];
+    totalRecovered = json['recovered'];
+    totalDeaths = json['deaths'];
+    todayCases = json['todayCases'];
+    todayDeaths = json['todayDeaths'];
+    totalTests = json['tests'];
+    population = json['population'];
+    countriesNames = json['countries'];
+  }
 
-  final int totalCases;
-  final int activeCases;
-  final int totalRecovered;
-  final int totalDeaths;
-  final int todayCases;
-  final int todayDeaths;
-  final int totalTests;
-  final int population;
-  final List<Country> countries;
+  String name;
+  int totalCases;
+  int activeCases;
+  int totalRecovered;
+  int totalDeaths;
+  int todayCases;
+  int todayDeaths;
+  int totalTests;
+  int population;
+  List countriesNames;
 
   double get activeCasesRate => activeCases / totalCases * 100.0;
   double get recoveredCasesRate => totalRecovered / totalCases * 100.0;
