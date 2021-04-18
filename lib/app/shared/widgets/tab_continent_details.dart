49344deb97b680cf2732d4bf2073409f2a5fef48
MSavioti <angelosavioti@gmail.com>
2021-04-18 02:35:30 -0300

    adicionando separação entre itens na coluna

--- lib/app/shared/widgets/tab_continent_details.dart
+++ lib/app/shared/widgets/tab_continent_details.dart
@@ -10,143 +10,157 @@ class ContinentDetailsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Container(
-                  color: Colors.white,
-                  width: 345.0,
-                  height: 246.0,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          TitleText(
-                            text: 'Total',
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          LabeledValueText(
-                            labelText: 'Total de Casos',
-                            valueText: 'teste',
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          LabeledValueText(
-                            labelText: 'Ativos',
-                            valueText: '27%',
-                            valueFontSize: 22.0,
-                            valueFontColor: Color(0xFF4461C2),
-                            valueFontWeight: FontWeight.bold,
-                            containerHeight: 51.0,
-                          ),
-                          LabeledValueText(
-                            labelText: 'Curados',
-                            valueText: '27%',
-                            valueFontSize: 22.0,
-                            valueFontColor: Color(0xFF5FD92B),
-                            valueFontWeight: FontWeight.bold,
-                            containerHeight: 51.0,
-                          ),
-                          LabeledValueText(
-                            labelText: 'Óbitos',
-                            valueText: '27%',
-                            valueFontSize: 22.0,
-                            valueFontColor: Color(0xFFFF2665),
-                            valueFontWeight: FontWeight.bold,
-                            containerHeight: 51.0,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-            Row(
-              children: [
-                Container(
-                  color: Colors.white,
-                  width: 345.0,
-                  height: 156.0,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          TitleText(
-                            text: 'Hoje',
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          LabeledValueText(
-                            labelText: 'Casos',
-                            valueText: '+ 50000',
-                          ),
-                          LabeledValueText(
-                            labelText: 'Óbitos',
-                            valueText: '- 1000',
-                            valueFontColor: Color(0xFFFF2665),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-            Row(
-              children: [
-                Container(
-                  color: Colors.white,
-                  width: 345.0,
-                  height: 159.0,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          TitleText(
-                            text: 'Testes',
-                          ),
-                        ],
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          LabeledValueText(
-                            labelText: 'Realizados',
-                            valueText: '30000',
-                          ),
-                          LabeledValueText(
-                            labelText: 'População',
-                            valueText: '100000',
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ],
-        ),
+      padding: EdgeInsets.fromLTRB(15.0, 21.0, 15.0, 21.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Container(
+                color: Colors.white,
+                width: 325.0,
+                height: 246.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TitleText(
+                          text: 'Total',
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        LabeledValueText(
+                          labelText: 'Total de Casos',
+                          valueText: 'teste',
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        LabeledValueText(
+                          labelText: 'Ativos',
+                          valueText: '27%',
+                          valueFontSize: 22.0,
+                          valueFontColor: Color(0xFF4461C2),
+                          valueFontWeight: FontWeight.bold,
+                          containerHeight: 51.0,
+                        ),
+                        LabeledValueText(
+                          labelText: 'Curados',
+                          valueText: '27%',
+                          valueFontSize: 22.0,
+                          valueFontColor: Color(0xFF5FD92B),
+                          valueFontWeight: FontWeight.bold,
+                          containerHeight: 51.0,
+                        ),
+                        LabeledValueText(
+                          labelText: 'Óbitos',
+                          valueText: '27%',
+                          valueFontSize: 22.0,
+                          valueFontColor: Color(0xFFFF2665),
+                          valueFontWeight: FontWeight.bold,
+                          containerHeight: 51.0,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                height: 25.0,
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                color: Colors.white,
+                width: 325.0,
+                height: 156.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TitleText(
+                          text: 'Hoje',
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        LabeledValueText(
+                          labelText: 'Casos',
+                          valueText: '+ 50000',
+                        ),
+                        LabeledValueText(
+                          labelText: 'Óbitos',
+                          valueText: '- 1000',
+                          valueFontColor: Color(0xFFFF2665),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: [
+              SizedBox(
+                height: 25.0,
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                color: Colors.white,
+                width: 325.0,
+                height: 159.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TitleText(
+                          text: 'Testes',
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        LabeledValueText(
+                          labelText: 'Realizados',
+                          valueText: '30000',
+                        ),
+                        LabeledValueText(
+                          labelText: 'População',
+                          valueText: '100000',
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
