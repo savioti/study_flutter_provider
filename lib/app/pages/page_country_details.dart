2d9b4a3743d4957945a9130661c9a7599e555672
MSavioti <angelosavioti@gmail.com>
2021-04-18 04:53:21 -0300

    trocando espaços na url por %20 para evitar erros

--- lib/app/pages/page_country_details.dart
+++ lib/app/pages/page_country_details.dart
@@ -8,8 +8,11 @@ class CountryDetailsPage extends StatelessWidget {
   const CountryDetailsPage({Key key}) : super(key: key);
 
   Future<Country> _httpGet(String countryName) async {
+    String countryNameUrl = countryName.toLowerCase();
+    countryNameUrl = countryNameUrl.replaceAll(' ', '%20');
+
     var response = await Dio().get(
-        'https://disease.sh/v3/covid-19/countries/${countryName.toLowerCase()}?strict=true');
+        'https://disease.sh/v3/covid-19/countries/$countryNameUrl?strict=true');
 
     if (response == null) return null;
     var jsonContent = response.data;
