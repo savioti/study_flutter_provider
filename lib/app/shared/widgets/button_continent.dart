aa4f142baefd633594f5218ba6e8a5884b4f2733
MSavioti <angelosavioti@gmail.com>
2021-04-18 15:38:54 -0300

    extraindo list tile do continente

--- lib/app/shared/widgets/button_continent.dart
+++ lib/app/shared/widgets/button_continent.dart
@@ -1,30 +1,12 @@
 import 'package:desafio_covid/app/shared/models/continent.dart';
 import 'package:desafio_covid/app/shared/widgets/boxdecoration_white_with_shadow.dart';
+import 'package:desafio_covid/app/shared/widgets/list_tile_continent.dart';
 import 'package:flutter/material.dart';
 
 class ContinentButton extends StatelessWidget {
   const ContinentButton({Key key, @required this.continent}) : super(key: key);
 
   final Continent continent;
-
-  String _findContinentIcon(String name) {
-    switch (name) {
-      case 'Asia':
-        return 'assets/images/icon_asia_44x44.png';
-      case 'North America':
-        return 'assets/images/icon_america_44x44.png';
-      case 'South America':
-        return 'assets/images/icon_america_44x44.png';
-      case 'Europe':
-        return 'assets/images/icon_europe_44x44.png';
-      case 'Africa':
-        return 'assets/images/icon_africa_44x44.png';
-      case 'Australia/Oceania':
-        return 'assets/images/icon_oceania_44x44.png';
-      default:
-        throw ArgumentError('$name is not a continent.');
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,41 +34,7 @@ class ContinentButton extends StatelessWidget {
             Navigator.pushNamed(context, '/continent_overview',
                 arguments: continent),
           },
-          // TODO: extrair widget
-          child: ListTile(
-            contentPadding: EdgeInsets.fromLTRB(0.0, 3.0, 0.0, 4.0),
-            leading: CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 22.0,
-              child: Image.asset(
-                _findContinentIcon(continent.name),
-              ),
-            ),
-            title: Text(
-              continent.name,
-              style: TextStyle(
-                fontFamily: 'Ubuntu',
-                fontStyle: FontStyle.normal,
-                fontSize: 13.0,
-                color: Color(0xFF1E2243),
-              ),
-            ),
-            subtitle: Text(
-              '${continent.countriesCount} países',
-              style: TextStyle(
-                fontFamily: 'Ubuntu',
-                fontStyle: FontStyle.normal,
-                fontSize: 12.0,
-                color: Color(0xFF969AA8),
-              ),
-            ),
-            // TODO: extrair widget
-            trailing: Icon(
-              Icons.arrow_forward_ios,
-              size: 16,
-              color: Color(0xFFCCCCCC),
-            ),
-          ),
+          child: ContinentListTile(continent: continent),
         ),
       ),
     );
---
+++ lib/app/shared/widgets/list_tile_continent.dart
@@ -0,0 +1,68 @@
+import 'package:desafio_covid/app/shared/models/continent.dart';
+import 'package:flutter/material.dart';
+
+class ContinentListTile extends StatelessWidget {
+  const ContinentListTile({
+    Key key,
+    @required this.continent,
+  }) : super(key: key);
+
+  final Continent continent;
+
+  String _findContinentIcon(String name) {
+    switch (name) {
+      case 'Asia':
+        return 'assets/images/icon_asia_44x44.png';
+      case 'North America':
+        return 'assets/images/icon_america_44x44.png';
+      case 'South America':
+        return 'assets/images/icon_america_44x44.png';
+      case 'Europe':
+        return 'assets/images/icon_europe_44x44.png';
+      case 'Africa':
+        return 'assets/images/icon_africa_44x44.png';
+      case 'Australia/Oceania':
+        return 'assets/images/icon_oceania_44x44.png';
+      default:
+        throw ArgumentError('$name is not a continent.');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.fromLTRB(0.0, 3.0, 0.0, 4.0),
+      leading: CircleAvatar(
+        backgroundColor: Colors.white,
+        radius: 22.0,
+        child: Image.asset(
+          _findContinentIcon(continent.name),
+        ),
+      ),
+      title: Text(
+        continent.name,
+        style: TextStyle(
+          fontFamily: 'Ubuntu',
+          fontStyle: FontStyle.normal,
+          fontSize: 13.0,
+          color: Color(0xFF1E2243),
+        ),
+      ),
+      subtitle: Text(
+        '${continent.countriesCount} países',
+        style: TextStyle(
+          fontFamily: 'Ubuntu',
+          fontStyle: FontStyle.normal,
+          fontSize: 12.0,
+          color: Color(0xFF969AA8),
+        ),
+      ),
+      // TODO: extrair widget
+      trailing: Icon(
+        Icons.arrow_forward_ios,
+        size: 16,
+        color: Color(0xFFCCCCCC),
+      ),
+    );
+  }
+}
