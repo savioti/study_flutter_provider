4ae0f81ed1849886e45bb8e92f464bf0c238eb36
MSavioti <angelosavioti@gmail.com>
2021-04-18 03:31:01 -0300

    criada tab de listagem de países

--- lib/app/pages/page_continent_overview.dart
+++ lib/app/pages/page_continent_overview.dart
@@ -3,6 +3,7 @@ import 'package:desafio_covid/app/shared/widgets/appbar_no_icon.dart';
 import 'package:desafio_covid/app/shared/widgets/button_return.dart';
 import 'package:desafio_covid/app/shared/widgets/center_error_text.dart';
 import 'package:desafio_covid/app/shared/widgets/tab_continent_details.dart';
+import 'package:desafio_covid/app/shared/widgets/text_title.dart';
 import 'package:flutter/material.dart';
 
 class ContinentOverviewPage extends StatelessWidget {
@@ -29,8 +30,11 @@ class ContinentOverviewPage extends StatelessWidget {
         'China',
         'Indonésia',
         'Taiwan',
-        'ìndia',
-        'Afeganistão'
+        'Índia',
+        'Afeganistão',
+        'Síria',
+        'Armenia',
+        'Georgia'
       ];
     }
 
@@ -92,8 +96,72 @@ class ContinentOverviewPage extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
-            ContinentDetailsTab(),
-            Column(),
+            ContinentDetailsTab(
+              continent: continent,
+            ),
+            ListView.separated(
+              padding: EdgeInsets.fromLTRB(14.5, 19.5, 14.5, 19.5),
+              itemCount: continent.countriesCount,
+              itemBuilder: (_, i) {
+                return ButtonTheme(
+                  child: Container(
+                    height: 55.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(4.0),
+                      ),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Color(0x0700000D),
+                          offset: Offset(0.0, 4.0),
+                          // spreadRadius: 3.0,
+                          blurRadius: 6.0,
+                        ),
+                      ],
+                    ),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.white),
+                        shape: MaterialStateProperty.all(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(4.0),
+                            ),
+                          ),
+                        ),
+                        elevation: MaterialStateProperty.all(0.0),
+                      ),
+                      onPressed: () => {},
+                      child: ListTile(
+                        contentPadding:
+                            EdgeInsets.fromLTRB(11.0, 3.0, 0.0, 4.0),
+                        leading: Text(
+                          continent.countriesNames[i],
+                          style: TextStyle(
+                            fontFamily: 'Ubuntu',
+                            fontStyle: FontStyle.normal,
+                            fontSize: 15.0,
+                            color: Color(0xFF1E2243),
+                          ),
+                        ),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios_rounded,
+                          size: 16,
+                          color: Color(0xFFCCCCCC),
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (_, __) {
+                return Divider(
+                  height: 17.0,
+                  color: Color(0x00000000),
+                );
+              },
+            ),
           ],
         ),
       ),
