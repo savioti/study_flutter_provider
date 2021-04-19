2db5a7c4fb90388f8b961384a39c7d6d260ba95d
MSavioti <angelosavioti@gmail.com>
2021-04-18 23:40:53 -0300

    removendo TODO

--- lib/app/shared/widgets/button_return.dart
+++ lib/app/shared/widgets/button_return.dart
@@ -8,7 +8,6 @@ class ReturnButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return IconButton(
-      // TODO: deixar setinha "grossa"
       icon: const Icon(Icons.arrow_back_ios_rounded),
       onPressed: () => Navigator.of(context).pop(),
       iconSize: 19.0,
